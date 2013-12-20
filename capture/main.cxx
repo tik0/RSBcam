c3fac60930544e54605ab01b0573cefca3fd634b
tik0 <korthals.timo@gmail.com>
2013-12-20 01:10:08 +0100

    Cleanup code

--- capture/main.cxx
+++ capture/main.cxx
@@ -1,21 +1,15 @@
-//
-#include <iostream>
-//
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "MatConverter.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <MatConverter/MatConverter.hpp>
 
 #include <boost/shared_ptr.hpp>
-#include <rsb0.9/rsb/Factory.h>
-#include <rsb0.9/rsb/converter/Repository.h>
-#include <rsb0.9/rsb/Event.h>
-#include <rsb0.9/rsb/Handler.h>
-#include <rsb0.9/rsb/filter/OriginFilter.h>
+#include <rsb/Factory.h>
+#include <rsb/converter/Repository.h>
+#include <rsb/Event.h>
+#include <rsb/Handler.h>
+#include <rsb/filter/OriginFilter.h>
 
 
-
-//
-//
 using namespace boost;
 using namespace std;
 //using namespace cv;
@@ -23,16 +17,7 @@ using namespace rsb;
 using namespace converter_Mat;
 using namespace rsb::converter;
 
-const int KEY_ESC = 27;
-const int DELAY = 30;
-
-//const string WIN_NAME = "Camera View";
-
-//// Print out the recieved octree
-//void getMessage(rsb::EventPtr event);
-
 int main() {
-
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
   // Register our converter within the collection of converters for
@@ -41,17 +26,10 @@ int main() {
   shared_ptr<MatConverter> converter(new MatConverter());
   converterRepository<std::string>()->registerConverter(converter);
 
-
   rsb::Factory &factory = rsb::Factory::getInstance();
 
   // Create the informer
   Informer<cv::Mat>::Ptr informer = getFactory().createInformer<cv::Mat> (Scope("/image"));
-
-//  // Create and start the listener
-//  rsb::ListenerPtr listener = factory.createListener("/image");
-//  listener->addHandler(rsb::HandlerPtr(new rsb::EventFunctionHandler(&getMessage)));
-
-
   ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -63,27 +41,15 @@ int main() {
 //  cv::Mat frame;
   shared_ptr<cv::Mat> frame(new cv::Mat);
 
-  // Read the key
-//  int key = cv::waitKey(DELAY);
-
-  // Process the cam as long as it is open
-//  cam.isOpened()
+  // Process the cam forever
   for (; ;) {
 
     // Save the actual picture to the frame object
     cam >> *frame;
     cv::waitKey(1000);
-//    // Show the picture
-//    cv::imshow(WIN_NAME, frame);
     // Send the data.
-          informer->publish(frame);
+    informer->publish(frame);
 
-//    // Read a keystroke
-//    key = cv::waitKey(DELAY);
-//
-//    // If the keystroke was ESC, then break
-//    if (KEY_ESC == key)
-//      break;
   }
 
   // Free the cam
@@ -92,15 +58,3 @@ int main() {
   return 0;
 
 }
-
-
-//void getMessage(rsb::EventPtr event) {
-//
-//        // Get the data
-//    shared_ptr<cv::Mat> message_rec = static_pointer_cast<cv::Mat>(event->getData());
-//
-////    std::cout << "blah" << std::endl;
-//    // Write OcTree to file
-////    cv::imshow("Show It", *message_rec);
-////    cv::imwrite( "Image.jpg", *message_rec );
-//}
